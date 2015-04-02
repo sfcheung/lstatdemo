1998fb5ca3a3b700cf8105f86f618f5af84fdc33
Shu Fai Cheung <shufai.cheung@gmail.com>
2015-04-03 00:08:00 +0800

    Fix the documentation of rundemo
    Correct the parameter name.

--- R/rundemo.R
+++ R/rundemo.R
@@ -2,14 +2,14 @@
 #'
 #'@description This function run a shiny app demo in this package
 #'
-#'@param demo The name of the demo
+#'@param demoname The name of the demo
 #'
 #'@return The app will start in a web browser
 #'
 #'@examples
-#'
+#'\dontrun{
 #'rundemo("ANOVAResidual")
-#'
+#'}
 
 rundemo <- function(demoname) {
   shiny::runApp(system.file(demoname, package="lstatdemo"))
